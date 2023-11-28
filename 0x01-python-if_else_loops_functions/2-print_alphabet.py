acaa39527c3b7054be972d621403cbc0fb80a809
jaymfune <jacobmfune2@gmail.com>
2023-11-28 18:31:27 +0200

    fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(26):
-    if i % 2 == 0:
-        print('{:c}'.format(122 - i), end='')
-    else:
-        print('{:c}'.format(90 - i), end='')
+for ascii in range(97, 123):
+    print(chr(ascii), end='')
