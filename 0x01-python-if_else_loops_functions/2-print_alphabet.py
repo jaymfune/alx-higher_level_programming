c031a09b89ec39f493f8364ae408b4671a009a47
jaymfune <jacobmfune2@gmail.com>
2023-11-28 18:29:15 +0200

    fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for ascii in range(ord('a'), ord('z') + 1):
-    alphabet = chr(ascii);
-    print(alphabet, end='')
+for i in range(26):
+    if i % 2 == 0:
+        print('{:c}'.format(122 - i), end='')
+    else:
+        print('{:c}'.format(90 - i), end='')
