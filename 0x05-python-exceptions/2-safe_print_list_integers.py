0bff9e0a7a3293861d591223bc90c188e5cc4074
jaymfune <jacobmfune2@gmail.com>
2023-12-19 13:46:37 +0200

    remove space

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
         # Iterate up to the minimum of x and the length of my_list
         for i in range(x):
             if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end=" ")
+                print("{:d}".format(my_list[i]), end="")
                 count_integers += 1
         print()
         return count_integers
