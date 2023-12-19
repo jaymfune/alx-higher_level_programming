9a6f4ed8c68381b258b2aaa95f900efa25276162
jaymfune <jacobmfune2@gmail.com>
2023-12-19 13:33:35 +0200

    Fix index handling in safe_print_list function

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=None, x=0):
     try:
         if my_list is None:
             my_list = []
-        for i in range(x):
+        for i in range(min(x, len(my_list))):
             print(my_list[i], end="")
         print()
         return x
