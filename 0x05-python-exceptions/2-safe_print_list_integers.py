3e66d9f57a0a754711755b3459e68520cd47bc5b
jaymfune <jacobmfune2@gmail.com>
2023-12-19 13:45:27 +0200

    refactor 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,6 +2,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     try:
         count_integers = 0
+        # If my_list is not provided, initialize it as an empty list
+        if my_list is None:
+            my_list = []
+
+        # Iterate up to the minimum of x and the length of my_list
         for i in range(x):
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end=" ")
